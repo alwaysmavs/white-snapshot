92ab28f0239505e9c01e5eac727109dcdd31ca55
伍双 <beingswu@gmail.com>
2019-09-19 18:14:41 +0800

    fix bug

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ export default class SnapshotToolImplement {
 
     /**
      * 初始化截图类
-     * 
+     *
      * @param displayer 实时房间 Room 实例/回放房间 Player 实例
      * @param type 转换图片格式
      * @param quality 转换压缩质量
@@ -14,7 +14,7 @@ export default class SnapshotToolImplement {
 
     /**
      * 将特定的场景预览内容转成 Base64 图片
-     * 
+     *
      * @param scenePath 场景路径（场景目录+场景名）
      * @param width 宽
      * @param height 高
@@ -28,7 +28,7 @@ export default class SnapshotToolImplement {
 
     /**
      * 将特定的场景预览内容转成 Blob。https://mdn.io/toBlob
-     * 
+     *
      * @param div 进行渲染的 div
      * @param scenePath 需要进行截图的场景路径，可选
      */
@@ -40,9 +40,9 @@ export default class SnapshotToolImplement {
     }
 
     /**
-     * 
+     *
      * 创建一个符合宽高的div，承载预览场景
-     * 
+     *
      * @param width 宽
      * @param height 高
      */
@@ -61,7 +61,7 @@ export default class SnapshotToolImplement {
 
     /**
      * 将特定场景的预览内容，渲染到指定 div 上，然后返回 canvas 截图
-     * 
+     *
      * @param scenePath 场景路径（场景目录+场景名）
      * @param div 用来渲染场景的 div
      */
@@ -72,7 +72,7 @@ export default class SnapshotToolImplement {
 
     /**
      * 将 div 的内容绘制成 canvas
-     * 
+     *
      * @param div canvas 截图的目标 div
      */
     private async div2canvas(div: HTMLElement): Promise<HTMLCanvasElement> {
@@ -80,19 +80,18 @@ export default class SnapshotToolImplement {
             Array.from(div.getElementsByTagName("svg")).forEach(s => {
                 // https://github.com/eKoopmans/html2pdf.js/issues/185
                 // https://github.com/niklasvh/html2canvas/issues/1578
-                console.log("svg ", s.clientWidth, s.clientHeight);
-                s.setAttribute("width", `${s.clientWidth}`);
-                s.setAttribute("height", `${s.clientHeight}`);
+                // s.setAttribute("width", `${s.clientWidth}`);
+                // s.setAttribute("height", `${s.clientHeight}`);
             });
         }});
     }
 
     /**
-     * 将 canvas 转换为 Blob 对象 
-     * 
-     * @param canvas 
-     * @param type 
-     * @param quality 
+     * 将 canvas 转换为 Blob 对象
+     *
+     * @param canvas
+     * @param type
+     * @param quality
      */
     private async canvasToImage(canvas: HTMLCanvasElement): Promise<Blob> {
         return new Promise<Blob>((resolve) => {
